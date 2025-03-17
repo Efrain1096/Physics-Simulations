6b5c2cf0d01a4de75d9fd02ba55542057f89e289
Efrain1096 <Efrainmiguel@gmail.com>
2025-03-16 22:22:24 -0700

    The equations have been tested to work as expected. On the next commit, the air drag ones will be commented back in and the original ones out.

--- Computational_Physics_Programs/Chapter_2_Kinematics/Motion_on_the_Plane/Projectile/Projectile.cpp
+++ Computational_Physics_Programs/Chapter_2_Kinematics/Motion_on_the_Plane/Projectile/Projectile.cpp
@@ -141,13 +141,18 @@ int main()
             vx = v0x; 
             vy = v0y - g * t; 
         */
-            
+            x = v0x * t; 
+            y = v0y * t - 0.5 * g * t * t;
+            vx = v0x; 
+            vy = v0y - g * t; 
        
-
+       /*
         x = (v0x*t)*(1.0 - exp(-k*t));
         y = (1.0/k)*(v0y+(g/k))*(1.0-exp(-k*t))-(g/k)*t;
         vx = v0x*exp(-k*t);
         vy = (v0y+(g/k))*exp(-k*t)-(g/k);
+       */
+     
 
         file << t << " " << x << " " << y << " " << vx << " " << vy << " " << endl;
         cout << t << " " << x << " " << y << " " << vx << " " << vy << " " << endl;
